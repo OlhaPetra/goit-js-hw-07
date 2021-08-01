430f4e96a408651d8fcd878c442d48ad2deeea24
OlhaPetra <80599236+OlhaPetra@users.noreply.github.com>
2021-08-01 20:13:24 +0300

    task

---
+++ js/task-01.js
@@ -0,0 +1,30 @@
+const allCategoriesEl = document.querySelectorAll(".item");
+console.log(`В списке ${allCategoriesEl.length} категории.`);
+console.log(allCategoriesEl);
+
+const showCategories = allCategoriesEl.map(categoryEl => {
+    (const itemTitleEl = categoryEl.firstElementChild;
+    //console.log(`Категория: ${itemTitleEl.textContent}`);
+    )}
+showCategories()
+
+
+
+    //console.log(`Категория: ${categoryEl.firstElementChild.textContent}`))
+
+
+//for (let i = 0; i = allCategoriesEl.length; i++) {
+    //console.log(allCategoriesEl[i]);
+
+    //const itemTitleEl = allCategoriesEl[i]
+
+    //.firstElementChild;
+    //console.log(`Категория: ${itemTitleEl.textContent}`);
+    //const subcategoryItemEl = allCategoriesEl[i].lastElementChild;
+    //const subcategoryTitleItemEl = subcategoryItemEl.children;
+    //console.log(`Количество: ${subcategoryTitleItemEl.length}`);
+//};
+
+
+
+
