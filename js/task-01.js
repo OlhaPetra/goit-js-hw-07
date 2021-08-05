23defd9473cf0de0de2a65701219779d844c0ebc
OlhaPetra <80599236+OlhaPetra@users.noreply.github.com>
2021-08-05 23:52:54 +0300

    correct

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,7 @@
-//Посчитает и выведет в консоль количество категорий в ul#categories
 
 const allCategoriesEl = document.querySelectorAll(".item");
 console.log(`В списке ${allCategoriesEl.length} категории.`);
 
-//Для каждого элемента в списке найдет и выведет в консоль текст заголовка элемента и количество элементов в категории
 
 allCategoriesEl.forEach(categoriesEl => console.log(`Категория: ${categoriesEl.firstElementChild.textContent}
 Количество элементов:${categoriesEl.lastElementChild.children.length}`));
