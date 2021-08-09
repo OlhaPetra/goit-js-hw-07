3b62728790ecb251a088283c660703d7ea854b4e
OlhaPetra <80599236+OlhaPetra@users.noreply.github.com>
2021-08-09 20:01:43 +0300

    correct task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,17 +1,20 @@
 const inputEl = document.getElementById("validation-input");
 const maxInputText = inputEl.getAttribute("data-length");
+console.log(maxInputText)
+console.log(typeof maxInputText)
 
 inputEl.addEventListener("blur", onBlur);
 
 function onBlur(event) {
-  if (event.target.value.length > maxInputText) {
-    event.target.classList.remove("valid");
-    event.target.classList.add("invalid");
+  if (event.target.value.length === Number(maxInputText)) {
+    event.target.classList.add("valid");
+    event.target.classList.remove("invalid");
+   
   } else if (event.target.value === "") {
     event.target.classList.remove("valid");
     event.target.classList.remove("invalid");
   } else {
-    event.target.classList.remove("invalid");
-    event.target.classList.add("valid");
+    event.target.classList.remove("valid");
+    event.target.classList.add("invalid");
   }
 }
