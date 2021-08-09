97e587f2ab62f15d2f1d8573fa7d2da956ea56fb
OlhaPetra <80599236+OlhaPetra@users.noreply.github.com>
2021-08-09 20:12:48 +0300

    correct  task 7

--- js/task-07.js
+++ js/task-07.js
@@ -1,13 +1,8 @@
 const rangeInputEl = document.getElementById("font-size-control");
 const spanTextEl = document.getElementById("text");
 
-rangeInputEl.setAttribute("min", 0);
-rangeInputEl.setAttribute("max", 32);
-rangeInputEl.setAttribute("value", 16);
-
 rangeInputEl.addEventListener("input", onRangeInput);
 
-function onRangeInput(event) {
-  const inputValue = event.target.value;
-  spanTextEl.style.fontSize = `${inputValue}px`;
-}
+function onRangeInput() {
+  spanTextEl.setAttribute("style", `font-size: ${rangeInputEl.value}px`);
+};
